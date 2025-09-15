c19d63b5e8a0388cee3e424a0418228d0ca46c39
peterseninga <119743093+peterseninga@users.noreply.github.com>
2025-09-15 16:54:33 +0200

    feat: Supabase Integration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,7 +22,11 @@ export default defineNuxtConfig({
 
   css: ['@fontsource-variable/mulish', '@fontsource/marcellus', '~/assets/style.css'],
 
-  modules: ['@nuxt/eslint', '@nuxtjs/prismic', '@nuxtjs/tailwindcss'],
+  modules: ['@nuxt/eslint', '@nuxtjs/prismic', '@nuxtjs/tailwindcss', '@nuxtjs/supabase'],
+
+  supabase: {
+    redirect: false
+  },
 
   prismic: {
     endpoint: apiEndpoint || repositoryName,
