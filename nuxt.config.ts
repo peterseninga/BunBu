4ece8638de31daf458c74fc9f7e6521611f9bb88
peterseninga <119743093+peterseninga@users.noreply.github.com>
2025-10-07 13:05:57 +0200

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,7 +22,8 @@ export default defineNuxtConfig({
         { name: 'format-detection', content: 'telephone=no' },
       ],
       link: [
-        { rel: 'apple-touch-icon', href: '/favicon.png' },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
         { rel: 'icon', type: 'image/png', href: '/favicon.png' },
         { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
       ],
